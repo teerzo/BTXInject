5c2782fa45afe87f0cfff8f98162a8da2d57e8bc
Kyle McArthur <teerzo@live.com.au>
2015-12-03 12:08:57 +1100

    +added Current project to new github page

---
+++ js/objects_ver.js
@@ -0,0 +1,11 @@
+function LoadObjectList()
+{
+    var object_list = [   
+    {
+        "name": "Ver101",
+        "type": ".obj",
+        "path": "obj/VER101/VER101_",
+        //"diffuse": "obj/Arm/BoneDiffuse.png"
+    } ];
+    return object_list;
+ }
